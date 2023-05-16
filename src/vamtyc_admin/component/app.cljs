f5f25c6be47fdbdac62dc41d6b782a95d230776d
guillerglez88 <guillerglez88@gmail.com>
2023-05-16 21:33:33 +0200

    refactoring list component

--- src/vamtyc_admin/component/app.cljs
+++ src/vamtyc_admin/component/app.cljs
@@ -1,13 +1,7 @@
 (ns vamtyc-admin.component.app
   (:require
-   [vamtyc-admin.component.lib.panel :as pnl]
    [vamtyc-admin.component.resources.core :as res]
-   [vamtyc-admin.component.resources.request :as req]
    [vamtyc-admin.component.resources.list :as lst]))
 
 (defn app [res]
-  [:section {:class "section"}
-   [:div {:class "container"}
-    [pnl/panel
-     [req/render res/lookup (:filter res) {:placeholder "search"}]
-     [lst/render res/lookup (:main res)]]]])
+  [lst/render res/lookup (:main res)])
--- src/vamtyc_admin/component/resources/list.cljs
+++ src/vamtyc_admin/component/resources/list.cljs
@@ -1,7 +1,20 @@
-(ns vamtyc-admin.component.resources.list)
+(ns vamtyc-admin.component.resources.list
+  (:require
+   [reagent.core :as r]))
 
-(defn render [lookup res]
-  [:div
-   (for [item (:items res)]
-     (let [list-item (-> item :type lookup)]
-       ^{:key (:id item)} [list-item item]))])
+(defn render [lookup list]
+  (let [url (r/atom (:url list))
+        set-url #(reset! url (-> % .-target .-value))]
+    (fn [_lookup list attrs]
+      [:section {:class "list"}
+       [:header
+        [:form {:action @url :method (:method list)}]
+        [:input {:class "input"
+                 :type "text"
+                 :default-value @url
+                 :on-change set-url
+                 :placeholder (:placeholder attrs)}]]
+       [:section {:class "items"}
+        (for [item (:items list)]
+          (let [list-item (-> item :type lookup)]
+            ^{:key (:id item)} [list-item item]))]])))

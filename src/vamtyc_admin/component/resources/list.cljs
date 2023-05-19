c7faa604a15794f96d78d8bc876d0f0703951d1f
guillerglez88 <guillerglez88@gmail.com>
2023-05-19 19:52:13 +0200

    displaying navigation

--- src/vamtyc_admin/component/resources/list.cljs
+++ src/vamtyc_admin/component/resources/list.cljs
@@ -12,29 +12,48 @@
                         :headers {"Accept" "application/json"}})
         (.then #(-> % :body (js->clj :keywordize-keys true))))))
 
+(defn pagination [nav]
+  [:nav
+   [:a {:title "first"
+        :href (:first nav)}
+    [:span {:class "icon"}
+     [:i {:class "fa-solid fa-circle"}]]]
+   [:a {:title "prev"
+        :href (:prev nav)}
+    [:span {:class "icon"}
+     [:i {:class "fa-solid fa-caret-left"}]]]
+   [:a {:title "next"
+        :href (:next nav)}
+    [:span {:class "icon"}
+     [:i {:class "fa-solid fa-caret-right"}]]]
+   [:a {:title "last"
+        :href (:last nav)}
+    [:span {:class "icon"}
+     [:i {:class "fa-solid fa-circle"}]]]])
+
 (defn render [_lookup list _attrs]
-  (let [url (r/atom (:url list))
-        items (r/atom (:items list))
-        set-url #(reset! url (-> % .-target .-value))
+  (let [state (r/atom list)
+        set-url #(reset! state (assoc @state :url (-> % .-target .-value)))
         search #(do (.preventDefault %)
                     (-> (search %)
-                        (.then (fn [body]
-                                 (reset! url (:url body))
-                                 (reset! items (:items body))))))]
+                        (.then (fn [body] (reset! state body)))))]
     (fn [lookup _list attrs]
       [:section {:class "list"}
        [:header
-        [:form {:action @url
+        [:form {:action (:url @state)
                 :method :GET
                 :on-submit search}
          [:input {:type "text"
                   :name "url"
-                  :default-value @url
+                  :default-value (:url @state)
                   :on-change set-url
                   :placeholder (:placeholder attrs)}]]]
        [:section {:class "items"}
-        (for [item @items]
+        (for [item (:items @state)]
           (let [list-item (-> item :type lookup)]
             ^{:key (:id item)}
             [:div {:class "list-item"}
-             [list-item item]]))]])))
+             [list-item item]]))]
+       [:footer
+        [:p (str "total: " (:total @state))]
+        [pagination (:nav @state)]]])))
